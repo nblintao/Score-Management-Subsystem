b44274605f1ae43115585dd2300172ac16597700
Tao LIN <nblintao@126.com>
2015-06-14 15:00:43 +0800

    Chinese info

--- project2/tetris3D/dbtest/xls_utils.py
+++ project2/tetris3D/dbtest/xls_utils.py
@@ -135,7 +135,8 @@ def update_score(xlsx_filename, c_id):
     if c_id == xlsx_info.class_id:
         return temp_table_update(xlsx_info.class_id, xlsx_info.scores)
     else:
-        return "The class id mentioned in the uploaded file doesn't match your\
- selected class. Update failed."
+        # return "The class id mentioned in the uploaded file doesn't match your\
+ # selected class. Update failed."
+        return '文件中的课程编号与选择的不符，上传失败。'
 
 
